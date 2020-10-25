375f063c69f74ef335678a5668ddf5493bd41ae4
Bruno Medeiros <lesiano16@gmail.com>
2020-10-25 22:01:19 +0100

    minor refactoring; adding several unittests

--- mastermind_tests.py
+++ mastermind_tests.py
@@ -1,5 +1,6 @@
+import itertools
 import unittest
-from solver import generate_symbols
+from solver import generate_symbols, is_potential_match, remove_non_potential_matches
 
 
 class MasterMindTestCase(unittest.TestCase):
@@ -14,6 +15,68 @@ class MasterMindTestCase(unittest.TestCase):
     def test_max_symbols(self):
         symbols = generate_symbols(8)
         self.assertEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], symbols)
+
+    def test_guess_and_potential_match_all_wrong(self):
+        is_p_m = is_potential_match(correct_positions=0, correct_symbols=0, guess="abcd", potential_match="abcd")
+        self.assertFalse(is_p_m)
+
+    def test_guess_and_potential_match_all_right(self):
+        is_p_m = is_potential_match(correct_positions=4, correct_symbols=0, guess="abcd", potential_match="abcd")
+        self.assertTrue(is_p_m)
+
+    def test_guess_and_potential_match_symbols_correct(self):
+        is_p_m = is_potential_match(correct_positions=0, correct_symbols=4, guess="abcd", potential_match="abcd")
+        self.assertFalse(is_p_m)
+
+    def test_not_potential_match(self):
+        is_p_m_1 = is_potential_match(correct_positions=1, correct_symbols=3, guess="aaaa", potential_match="abcd")
+        self.assertFalse(is_p_m_1)
+
+    def test_potential_match(self):
+        is_p_m = is_potential_match(correct_positions=1, correct_symbols=0, guess="abcd", potential_match="aaaa")
+        self.assertTrue(is_p_m)
+
+    def test_potential_matches_with_only_one_possible_match(self):
+        valid_symbols = {'a', 'b', 'c'}
+        combinations = [p for p in itertools.product(valid_symbols, repeat=3)]
+        remove_non_potential_matches(3, 0, "abc", combinations)
+        self.assertEqual([('a', 'b', 'c')], combinations)
+
+    def test_potential_matches_impossible_case_1(self):
+        valid_symbols = {'a', 'b', 'c'}
+        combinations = [p for p in itertools.product(valid_symbols, repeat=3)]
+        print(combinations)
+        remove_non_potential_matches(0, 1, "aaa", combinations)
+        self.assertEqual([], combinations)
+
+    def test_potential_matches_impossible_case_2(self):
+        valid_symbols = {'a', 'b', 'c'}
+        combinations = [p for p in itertools.product(valid_symbols, repeat=3)]
+        print(combinations)
+        remove_non_potential_matches(0, 0, "abc", combinations)
+        self.assertEqual([], combinations)
+
+    def test_potential_matches_one_possible_match(self):
+        valid_symbols = {'a', 'b', 'c'}
+        combinations = [p for p in itertools.product(valid_symbols, repeat=3)]
+        print(combinations)
+        remove_non_potential_matches(3, 0, "abc", combinations)
+        self.assertEqual([('a', 'b', 'c')], combinations)
+
+    def test_potential_matches_two_possible_match(self):
+        valid_symbols = {'a', 'b', 'c'}
+        combinations = [p for p in itertools.product(valid_symbols, repeat=3)]
+        combinations.sort()
+        print(combinations)
+        remove_non_potential_matches(0, 3, "abc", combinations)
+        self.assertEqual([('b', 'c', 'a'), ('c', 'a', 'b')], combinations)
+
+    def test_potential_matches_three_possible_match(self):
+        valid_symbols = {'a', 'b', 'c'}
+        combinations = [p for p in itertools.product(valid_symbols, repeat=3)]
+        combinations.sort()
+        remove_non_potential_matches(1, 0, "abc", combinations)
+        self.assertEqual([('a', 'a', 'a'), ('b', 'b', 'b'), ('c', 'c', 'c')], combinations)
 
 
 if __name__ == '__main__':
--- solver.py
+++ solver.py
@@ -1,15 +1,15 @@
 import itertools
 
 
-def is_potential_match(correct_positions, correct_symbols, code, secret_code):
-    code_l = list(code)
-    secret_code_l = list(secret_code)
+def is_potential_match(correct_positions, correct_symbols, guess, potential_match):
+    guess = list(guess)
+    potential_match = list(potential_match)
     index = 0
     c_pos = 0
-    while index < len(secret_code_l):
-        if code_l[index] == secret_code_l[index]:
-            del code_l[index]
-            del secret_code_l[index]
+    while index < len(potential_match):
+        if guess[index] == potential_match[index]:
+            del guess[index]
+            del potential_match[index]
             c_pos = c_pos + 1
         else:
             index = index + 1
@@ -17,21 +17,21 @@ def is_potential_match(correct_positions, correct_symbols, code, secret_code):
         return False
 
     c_symbols = 0
-    for c in code_l:
-        if c in secret_code_l:
+    for c in guess:
+        if c in potential_match:
             c_symbols = c_symbols + 1
-            secret_code_l.remove(c)
+            potential_match.remove(c)
 
     return c_symbols == correct_symbols
 
 
-def filter_potential_matches(correct_positions, correct_symbols, code, list_of_combinations):
+def remove_non_potential_matches(correct_positions, correct_symbols, guess, combinations):
     index = 0
-    while index < len(list_of_combinations):
-        if is_potential_match(correct_positions, correct_symbols, code, list_of_combinations[index]):
+    while index < len(combinations):
+        if is_potential_match(correct_positions, correct_symbols, guess, combinations[index]):
             index = index + 1
         else:
-            del list_of_combinations[index]
+            del combinations[index]
 
 
 def generate_symbols(number):
@@ -51,7 +51,7 @@ def game(number_of_symbols, number_of_slots):
         guess = input("What was the guess?")
         correct_pos = int(input("How many positions are correct ?"))
         correct_symbol = int(input("How many symbols are correct, but on wrong position ?"))
-        filter_potential_matches(correct_pos, correct_symbol, guess, combinations)
+        remove_non_potential_matches(correct_pos, correct_symbol, guess, combinations)
         total_combinations = len(combinations)
         print("Total combinations : ", total_combinations)
         found = input("Did you find ?")
@@ -59,7 +59,7 @@ def game(number_of_symbols, number_of_slots):
 
 
 if __name__ == '__main__':
-    game(6, 4)
+    game(3, 3)
 
 
 
